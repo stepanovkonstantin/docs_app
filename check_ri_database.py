00050932e1e96b1ce095852d0b137ac39068790c
stepanovkonstantin <konstantinus.stepanovichus@gmail.com>
2022-07-13 16:24:41 +0300

    + classes + optimize

--- check_ri_database.py
+++ check_ri_database.py
@@ -2,47 +2,76 @@ import sqlite3
 import csv
 
 def get_string_ri(index, c):
+    data_out = {}
     get_select = c.execute('SELECT id, LOCATION_AREA_CODE, NETWORK_OPERATOR_CODE, HOST_CODE FROM DB_LAC_RI WHERE id = ?', [index])
-    return get_select
+    for row in get_select:
+        data_out = {'lac': row[1], 'netopcode': row[2], 'host_code': row[3]}
+        print(data_out['netopcode'])
+    return data_out
 
 def get_location_info(lac, c):
-    get_select = c.execute('SELECT * FROM DB_LAC_ST0925 WHERE (LAC_Start <= ? AND Lac_End >= ?)', (lac, lac))
-    return get_select
+    data_out = {}
+    get_select = c.execute('select * FROM db_st0925 ds where (zone_code_start <= ? AND zone_code_end >= ?)', (lac, lac))
+    for row in get_select:
+        data_out = {'region_name' : row[0], 'zone_type' : row[1], 'zone_start' : row[2], 'zone_end' : row[3]}
+    return data_out
+
+def get_zone_info(zone_name, c):
+    zone_info = {}
+    opcode = ''
+    get_select_opcode = c.execute('select network_operator from regions2netopcode where region_name like ?', [zone_name])
+    for row in get_select_opcode:
+        opcode = str(row[0])
+        print(opcode)
+    get_select_zone_info = c.execute('select * from rus_regions where provider like ?', [opcode])
+    for row in get_select_zone_info:
+        zone_info = {'provider': row[0], 'sdp_zone': row[2], 'sdp_region_name': row[1], 'macroregion': row[4]}
+    return zone_info, opcode
 
 def put_location_list(location_file, location_data):
-    with open(location_file, 'a') as f:
+    with open(location_file, 'a', newline='') as f:
         write = csv.writer(f, delimiter=';', lineterminator="\n")
-        write.writerow(location_data)
+        write.writerows(location_data)
     return
 
 def main_cycle(count_i, c, out_file):
+    location_info_out = []
     with open(out_file, "w") as f:
         write = csv.writer(f, delimiter=';', lineterminator="\n")
-        write.writerow(["LAC", "NETWORK_OPERATOR_CODE", "HOST_ID", "STATUS", "REF_NETWORK_OPERATOR_CODE", "REGION_NAME", "LAC_START", "LAC_END", "ZONE_CODE_TYPE"])
+        write.writerow(["RI_LAC", "RI_NETWORK_OPERATOR_CODE", "RI_HOST_CODE", "STATUS", "REGION_NAME", "ZONE_CODE_TYPE", "LAC_START", "LAC_END", "SDP_PROVIDER", "SDP_REGION", "SDP_REGION_NAME", "SDP_MACROREGION"])
     count_i = count_i + 1
     for index in range(1, count_i):
-        get_select_ri = get_string_ri(str(index), c)
-        for row in get_select_ri:
-            location_data_out = [row[1], row[3], row[2]]
-        loc_info = get_location_info(location_data_out[0], c)
-        for row in loc_info:
-            if row[0] == None:
-                location_data_out.append("non-existent")
-            if str(location_data_out[2]) == str(row[1]):
-                location_data_out.append("ok")
-                location_data_out.append(row[1])
-                location_data_out.append(row[0])
-                location_data_out.append(row[3])
-                location_data_out.append(row[4])
-                location_data_out.append(row[2])
+        select_zone_info = {}
+        select_ri = get_string_ri(str(index), c)
+        select_locinfo = get_location_info(select_ri['lac'], c)
+        if 'region_name' in select_locinfo.keys():
+            select_zone_info, select_opcode = get_zone_info(select_locinfo['region_name'], c)
+            if str(select_ri['netopcode']).lower() == select_opcode.lower():
+                select_ri['status'] = "ok"
             else:
-                location_data_out.append("delete")
-                location_data_out.append(row[1])
-                location_data_out.append(row[0])
-                location_data_out.append(row[3])
-                location_data_out.append(row[4])
-                location_data_out.append(row[2])
-        put_location_list(out_file, location_data_out)
+                select_ri['status'] = "delete"
+        else:
+            select_ri['status'] = "non-existent"
+            select_locinfo = {'region_name' : 'n/a', 'zone_type' : 'n/a', 'zone_start' : 'n/a', 'zone_end' : 'n/a'}
+            select_zone_info = {'provider': 'n/a', 'sdp_zone': 'n/a', 'sdp_region_name': 'n/a', 'macroregion': 'n/a'}
+
+        location_info_str = [
+            select_ri['lac'],
+            select_ri['netopcode'],
+            select_ri['host_code'],
+            select_ri['status'],
+            select_locinfo.get('region_name'),
+            select_locinfo.get('zone_type'),
+            select_locinfo.get('zone_start'),
+            select_locinfo.get('zone_end'),
+            select_zone_info.get('provider'),
+            select_zone_info.get('sdp_zone'),
+            select_zone_info.get('sdp_region_name'),
+            select_zone_info.get('macroregion')
+        ]
+
+        location_info_out.append(location_info_str)
+    return location_info_out
 
 def main(db, out_file):
     global count_i
@@ -52,7 +81,8 @@ def main(db, out_file):
     count_strngs = c.execute('SELECT COUNT(*) FROM DB_LAC_RI')
     for row in count_strngs:
         count_i = int(row[0])
-    main_cycle(count_i, c, out_file)
+    location_info_out = main_cycle(count_i, c, out_file)
+    put_location_list(out_file, location_info_out)
 
 
 main('./db/locations.db', './saved/locations_ri_analyze.csv')
